7e011b5614f926d29f8c545c53d05ea7bf56e694
Shivamrathi001 <106222099+Shivamrathi001@users.noreply.github.com>
2022-06-24 13:09:32 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,8 +1,10 @@
-sudo yum update
-sudo yum install -y httpd.x86_64
-sudo systemctl start httpd.service
-sudo systemctl enable httpd.service
-sudo yum install php -y
-cd /opt/apps
-mv * /var/www/html
-# rm index.html
+# sudo yum update
+# sudo yum install -y httpd.x86_64
+# sudo systemctl start httpd.service
+# sudo systemctl enable httpd.service
+# sudo yum install php -y
+# cd /opt/apps
+# mv * /var/www/html
+# # rm index.html
+
+echo 'INstall complete'
